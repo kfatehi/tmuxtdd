290ae3404f0fa0e0599cf8b7cb649601ffe3d751
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-05-04 00:49:55 -0700

    ohhh very nice refactor

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # tmuxtdd
 
+reports test runner status in the tmux status bar by watching your files and executing your tests for you when they change
+
 ![tmuxtdd](https://raw.githubusercontent.com/kfatehi/tmuxtdd/master/demo.gif)
-
-## what
-
-A program that runs your test suite for you, capturing its exit status. It uses chokidar to watch files with respect to the glob pattern you pass in with `--watch`. By passing in `--status` you can query this status, however the output is designed for tmux.
 
 ## why
 
@@ -12,9 +10,9 @@ I wrote this program in order to see if I prefer using multiple tmux windows wit
 
 ## how
 
-The program comes bundled with a small tmux configuration that frequently calls `tmuxtdd --status` and renders test status to the top left of the tmux status bar. When you run `tmuxtdd`, this tmux configuration is sourced into your tmux session. Right before `tmuxtdd` exits, your original tmux configuration is sourced, restoring your environment.
+tmuxtdd reports test runner status in the status bar in this way:
 
-As a result, tmux becomes aware of your tests so you can keep your tests running in a different window and only shift over to the test runner when you see failure occurring.
+`tmux set-option -g status-left "Tests Passing"`
 
 ## installation
 
@@ -22,6 +20,6 @@ As a result, tmux becomes aware of your tests so you can keep your tests running
 
 ## usage
 
-Go to any node project and run a command such as this. You must have a `test` script defined in `package.json` because `tmuxtdd` is hardcoded to run `npm test`.
+example usage for a javascript project:
 
-`tmuxtdd --watch 'src/**/*.js,test/**/*.js'`
+`tmuxtdd run --watch 'src/**/*.js,test/**/*.js' --cmd 'npm test'`
--- index.js
+++ index.js
@@ -1,10 +1,71 @@
-module.exports = {
-  mocha: {
-    before: function() {
-    },
-    after: function() {
-      console.log(this);
-      
-    }
+var spawn = require('child_process').spawn;
+var chokidar = require('chokidar');
+var homedir = require('homedir');
+var fs = require('fs');
+var path = require('path');
+
+module.exports.originalConf = path.resolve(homedir(), '.tmux.conf');
+module.exports.tmuxConf = path.resolve(__dirname, 'tmux-tdd.conf');
+
+function getStatusLine(status) {
+  switch (status) {
+    case 'passing':
+      return '#[bg=green]#[fg=black] PASS ';
+    case 'failing':
+      return '#[bg=red]#[fg=black] FAIL ';
+    default: 
+      return '#[bg=black]#[fg=grey] ... ';
   }
+}
+
+function update(state) {
+  var statusLine = getStatusLine(state.status);
+  return spawn('tmux', ['set-option', '-g', 'status-left', statusLine], { stdio: 'inherit' })
+}
+
+
+function runner(cmd) {
+  var busy = false;
+  return function() {
+    if (busy) return null;
+    busy = true;
+    update({ status: 'busy' }).on('exit', function() {
+      var proc = spawn('bash', ['-c', cmd], {stdio: "inherit"});
+      proc.on('exit', function(status) {
+        update({ status: status === 0 ? 'passing' : 'failing' }).on('exit', function() {
+          busy = false;
+        })
+      });
+    })
+  }
+}
+
+module.exports.startRunner = function startRunner(watch, cmd) {
+  console.log('Starting runner');
+  var runTests = runner(cmd)
+  if (watch) {
+    var watchPattern = watch.split(',');
+    console.log('Watching with pattern:', watchPattern);
+    var watcher = chokidar.watch(watchPattern, {
+      ignored: /[\/\\]\./,
+      persistent: true
+    });
+    watcher
+    .on('add', runTests)
+    .on('change', runTests)
+    .on('unlink', runTests)
+  } else {
+    runTests()
+  }
+}
+
+module.exports.sourceTmuxConfig = function sourceTmuxConfig(filepath, callback) {
+  console.log('Sourcing tmux config '+filepath);
+  var proc = spawn('tmux', ['source-file', filepath], {stdio: "inherit"});
+  proc.on('exit', function(status) {
+    if (callback) {
+      if (status === 0) return callback();
+      else return callback(new Error('Non-zero exit status'));
+    }
+  })
 }
